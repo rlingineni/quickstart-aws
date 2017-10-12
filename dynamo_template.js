3e01cb3e3ed8524a772093f596d6670f7d831dcc
Raviteja Lingineni <rlingineni@live.com>
2017-10-12 12:23:07 -0500

    Update dynamo_template.js

--- dynamo_template.js
+++ dynamo_template.js
@@ -1,48 +1,73 @@
 var AWS = require('aws-sdk');
 
-exports.myHandler = function(event, context, callback) {
-  switch(event.httpMethod) {
-  case 'GET':
-    return handleRead(event.queryStringParameters, callback);
-  case 'POST':
-  case 'PUT':
-    return handleCreate(event.queryStringParameters, event.body, callback);
-  case 'DELETE':
-    return handleDelete(event.queryStringParameters, callback);
-  case 'UPDATE':
-    return handleUpdate(event.queryStringParameters, event.body, callback);
-  default:
-    callback(new Error('Invalid HTTP request method: ' + event.httpMethod));
-  }
-};
-
 const docClient = new AWS.DynamoDB.DocumentClient();
 
 const success = function(msg) {
-  let resp = {
+    
+    if(typeof msg !== "string" ){
+       msg = JSON.stringify(msg);  
+    }
+
+  var resp = {
     statusCode: 200,
+    headers:{
+      "Access-Control-Allow-Origin":"*"
+    },
     body: msg
   };
   return [null, resp];
 };
 
 const failure = function(why) {
-  let err = new Error(why);
-  let resp = {
+
+    if(typeof why !== "string" ){
+        why = JSON.stringify(why);  
+    }
+
+  var resp = {
     statusCode: 400,
-    body: err;
+    headers:{
+      "Access-Control-Allow-Origin":"*"
+    },
+    body: why
   };
-  return [err, resp];
+  return [null, resp];
 };
 
+const tableName = "{0}"
+const tableKey = "{1}"
+
+exports.myHandler = function(event, context, callback) {
+  
+switch(event.httpMethod) {
+  case 'GET':
+    handleRead(event.queryStringParameters);
+    break;
+  case 'POST':
+  case 'PUT':
+    handleCreate(event.queryStringParameters, event.body);
+    break;
+  case 'DELETE':
+    handleDelete(event.queryStringParameters);
+    break;
+  case 'UPDATE':
+    handleUpdate(event.queryStringParameters, event.body);
+    break;
+  default:
+    callback(new Error('Invalid HTTP request method: ' + event.httpMethod));
+  }
+  
 /// add the object described by the request body to the database
-function handleCreate(params, body, callback) {
+function handleCreate(params, body) {
+
+  console.log("Creating a New Item with ", body)
   try {
     let item = JSON.parse(body);
     let dynamoParams = {
-      TableName: '{{tableName}}',
+      TableName: tableName,
       Item: item
     };
+
     docClient
       .put(
         dynamoParams,
@@ -50,23 +75,30 @@ function handleCreate(params, body, callback) {
           let response;
           if (error) {
             response = failure(error);
+            callback(...response);
           } else {
             response = success(data);
+            callback(...response);
           }
-          callback(...response);
+          
         });
   } catch(error) {
+     console.log(error);
     callback(new Error('Error parsing POST parameters: ' + error));
   }
+  
 }
 
 /// fetch an object based on its key
-function handleRead(params, callback) {
-  let dynamoParams = {
-    TableName: '{{tableName}}',
-    Key: {{KeyObject}}
-  };
-  promise =
+function handleRead(params) {
+    let dynamoParams = {
+        TableName: tableName,
+        };
+
+        var key = {};
+        key[tableKey] = params[tableKey];
+        dynamoParams.Key = key;
+
     docClient
       .get(
         dynamoParams,
@@ -74,20 +106,23 @@ function handleRead(params, callback) {
           let response;
           if (error) {
             response = failure(error);
+            callback(...response);
           } else {
             response = success(data);
+            console.log(response);
+            callback(...response);
           }
-          callback(...response);
+          
         });
 }
 
 /// update or create an object
-function handleUpdate(params, body, callback) {
+function handleUpdate(params, body) {
   try {
     let item = JSON.parse(body);
     /// successfully parsed the request body
     let dynamoParams = {
-      TableName: '{{tableName}}',
+      TableName: tableName,
       Item: body
     };
     promise =
@@ -98,10 +133,12 @@ function handleUpdate(params, body, callback) {
             let response;
             if (error) {
               response = failure(error);
+              callback(...response);
             } else {
-              response = success(data);
+              response = success({msg:"Updated Information"});
+              callback(...response);
             }
-            callback(...response);
+            
           });
   } catch(err) {
     callback(new Error('Error parsing POST parameters: '))
@@ -110,22 +147,37 @@ function handleUpdate(params, body, callback) {
 }
 
 /// delete an object
-function handleDelete(params, callback) {
-  let dynamoParams = {
-    TableName: '{{tableName}}',
-    Key: {{keyObject}}
-  };
-  promise =
-    docClient
-      .delete(
-        dynamoParams,
-        function(error, data) {
-          let response;
-          if (error) {
-            response = failure(error);
-          } else {
-            response = success(data);
-          }
-          callback(...response);
-        });
+function handleDelete(params) {
+  
+    let dynamoParams = {
+        TableName: tableName,
+        };
+
+        var key = {};
+        key[tableKey] = params[tableKey];
+        dynamoParams.Key = key;
+
+        docClient
+            .delete(
+            dynamoParams,
+            function(error, data) {
+                let response;
+                if (error) {
+                console.log(error);
+                response = failure(error);
+                callback(...response);
+                } else {
+                response = success({msg:"Deleted!"});
+                callback(...response);
+                }
+            });
 }
+
+
+}
+
+
+
+
+
+
